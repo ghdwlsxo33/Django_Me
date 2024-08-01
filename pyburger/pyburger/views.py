bee091fc0a40a94f495f4cf65c804c37b29cdac1
HongJintae <ghdwlsxo33@gmail.com>
2024-08-01 10:28:36 +0900

    first commit

---
+++ pyburger/pyburger/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def main(request):
+    # return HttpResponse("Hello World")
+    return render(request, 'main.html')
+
+def main2(request):
+    # return HttpResponse("오늘 점심 밖에 안 나갈란다")
+    return render(request, 'main2.html')
